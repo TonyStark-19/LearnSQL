f8754267bde9c8d00882002e8bfe51b8718716d1
Aditya chandel <adityachandel371@gmail.com>
2025-06-10 13:26:03 +0530

    Started working on project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Use state and use effect
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// Mainpage
+export default function MainPage() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full h-screen flex flex-row">
+      <Left />
+      <Right />
+    </div>
   )
 }
 
-export default App
+// left column for pages list
+function Left() {
+
+  const [isDark, setIsDark] = useState(() => {
+    // initialize from localStorage
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDark]);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <div className="w-96 h-full fixed py-3 px-5
+      border-r-white border-3 bg-white dark:bg-slate-900/95">
+      <div className='flex justify-between flex-row'>
+        <div className="text-2xl font-semibold
+      text-white">LearnSQL</div>
+
+        <button
+          onClick={toggleTheme}
+          className="px-3 py-1 rounded-md
+                text-stone-800 dark:text-white border border-stone-600 dark:border-gray-800
+                hover:bg-stone-600 hover:text-white dark:hover:bg-gray-800 transition"
+        >
+          {isDark ? '🌞 Light' : '🌙 Dark'}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+// right column for pages
+function Right() {
+  return (
+    <div className="w-full
+    bg-white dark:bg-slate-900/95"></div>
+  )
+}
