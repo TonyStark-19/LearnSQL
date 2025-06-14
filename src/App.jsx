ec28cab74e8897234d305dd97cb8ad6a4b66390f
Aditya chandel <adityachandel371@gmail.com>
2025-06-14 21:15:51 +0530

    Added routing for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import { Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 // import pages
-import { Page1 } from "./pages";
-import { Page2 } from "./pages";
+import {
+  Page1, Page2, Page3, Page4, Page5, Page6, Page7, Page8, Page9, Page10, Page11, Page12, Page13, Page14,
+  Page15, Page16, Page17, Page18, Page19, Page20, Page21, Page22, Page23, Page24, Page25, Page26, Page27
+} from "./pages";
 
-// main export
+// main app
 export default function App() {
   return (
     <Routes>
@@ -35,6 +37,31 @@ function MainPage() {
           <Routes>
             <Route path="sql-intro" element={<Page1 />} />
             <Route path="dbms" element={<Page2 />} />
+            <Route path="mysql" element={<Page3 />} />
+            <Route path="create-DB" element={<Page4 />} />
+            <Route path="drop-DB" element={<Page5 />} />
+            <Route path="create-table" element={<Page6 />} />
+            <Route path="datatypes" element={<Page7 />} />
+            <Route path="commands" element={<Page8 />} />
+            <Route path="queries-DB" element={<Page9 />} />
+            <Route path="queries-tables" element={<Page10 />} />
+            <Route path="keys" element={<Page11 />} />
+            <Route path="constraints" element={<Page12 />} />
+            <Route path="select" element={<Page13 />} />
+            <Route path="where-cl" element={<Page14 />} />
+            <Route path="operators" element={<Page15 />} />
+            <Route path="limit-cl" element={<Page16 />} />
+            <Route path="order-by" element={<Page17 />} />
+            <Route path="aggregate" element={<Page18 />} />
+            <Route path="group-by" element={<Page19 />} />
+            <Route path="having-cl" element={<Page20 />} />
+            <Route path="cascading" element={<Page21 />} />
+            <Route path="More-queries" element={<Page22 />} />
+            <Route path="joins" element={<Page23 />} />
+            <Route path="sub-queries" element={<Page24 />} />
+            <Route path="views" element={<Page25 />} />
+            <Route path="projects" element={<Page26 />} />
+            <Route path="links" element={<Page27 />} />
           </Routes>
 
           <div className="text-slate-400 py-3 pl-3 font-semibold">Made with ❤️ by Aditya chandel</div>
@@ -49,10 +76,12 @@ function Navbar() {
   return (
     <div className="w-full fixed p-3 flex flex-row items-center justify-between
       bg-[#0F172A] border-b-slate-700 border-b-2">
-      <div className="text-2xl font-semibold flex flex-row items-center gap-2 cursor-default
+      <Link to="/sql-intro">
+        <div className="text-2xl font-semibold flex flex-row items-center gap-2 cursor-pointer
         text-slate-100">LearnSQL
-        <GrMysql className="text-2xl" />
-      </div>
+          <GrMysql className="text-2xl" />
+        </div>
+      </Link>
 
       <a href="https://github.com/TonyStark-19/LearnSQL">
         <button className="bg-transparent py-1.5 px-3 border-2 border-slate-700 rounded-xl cursor-pointer">
@@ -72,30 +101,30 @@ const array = [
 
 // list of pages 2
 const array2 = [
-  { name: "Create Database" },
-  { name: "Drop Database" },
-  { name: "Create Table" },
-  { name: "SQL Datatypes" },
-  { name: "SQL Commands" },
-  { name: "Queries (Database)" },
-  { name: "Queries (Tables)" },
-  { name: "SQL Keys" },
-  { name: "SQL Constraints" },
-  { name: "SQL Select" },
-  { name: "Where Clause" },
-  { name: "SQL Operators" },
-  { name: "Limit Clause" },
-  { name: "Order By Clause" },
-  { name: "Aggregate Functions" },
-  { name: "Group By Clause" },
-  { name: "Having Clause" },
-  { name: "Cascading for FK" },
-  { name: "More Queries (Tables)" },
-  { name: "Joins in SQL" },
-  { name: "SQL Sub Queries" },
-  { name: "MySQL Views" },
-  { name: "SQL Projects" },
-  { name: "More Links" }
+  { name: "Create Database", path: "/create-DB" },
+  { name: "Drop Database", path: "/drop-DB" },
+  { name: "Create Table", path: "/create-table" },
+  { name: "SQL Datatypes", path: "/datatypes" },
+  { name: "SQL Commands", path: "/commands" },
+  { name: "Queries (Database)", path: "/queries-DB" },
+  { name: "Queries (Tables)", path: "/queries-tables" },
+  { name: "SQL Keys", path: "/keys" },
+  { name: "SQL Constraints", path: "/constraints" },
+  { name: "SQL Select", path: "/select" },
+  { name: "Where Clause", path: "/where-cl" },
+  { name: "SQL Operators", path: "/operators" },
+  { name: "Limit Clause", path: "/limit-cl" },
+  { name: "Order By Clause", path: "/order-by" },
+  { name: "Aggregate Functions", path: "/aggregate" },
+  { name: "Group By Clause", path: "/group-by" },
+  { name: "Having Clause", path: "/having-cl" },
+  { name: "Cascading for FK", path: "/cascading" },
+  { name: "More Queries (Tables)", path: "/More-queries" },
+  { name: "Joins in SQL", path: "/joins" },
+  { name: "SQL Sub Queries", path: "/sub-queries" },
+  { name: "MySQL Views", path: "/views" },
+  { name: "SQL Projects", path: "/projects" },
+  { name: "More Links", path: "/links" }
 ]
 
 // left column for pages list
@@ -119,11 +148,13 @@ function Left() {
       <div className="text-slate-400 pl-3 pt-4 font-semibold uppercase">Learn sql</div>
       <div className='pt-5 pr-7 pb-5 w-85'>
         {array2.map((arr, idx) => (
-          <li className='group list-none py-2 px-5 font-semibold text-xl
+          <Link to={arr.path} key={idx}>
+            <li className='group list-none py-2 px-5 font-semibold text-xl
           rounded-r-xl cursor-pointer flex flex-row items-center justify-between
             hover:bg-slate-800 text-slate-100' key={idx}>{arr.name}
-            <IoIosArrowForward className="text-transparent group-hover:text-slate-100" />
-          </li>
+              <IoIosArrowForward className="text-transparent group-hover:text-slate-100" />
+            </li>
+          </Link>
         ))}
       </div>
     </div>
