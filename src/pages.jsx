f6c95458f3d974e4b6a03a2f230e06a6520adb0f
Aditya chandel <adityachandel371@gmail.com>
2025-06-16 21:00:22 +0530

    Added more pages content

--- src/pages.jsx
+++ src/pages.jsx
@@ -152,18 +152,26 @@ export function Page4() {
                 <div className="text-4xl font-semibold">Creating our first Database</div>
 
                 <ul className="text-2xl list-disc pl-6 pb-2">
-                    <li className="pt-7">To create your first Database on MySQL write the following command on
+                    <li className="pt-7">To create your first Database on MySQL write the following code on your
                         MySQl Workbench :</li>
                 </ul>
 
-                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md my-3 w-[800px]">
                     CREATE DATABASE xyz;
                 </div>
 
-                <ul className="text-2xl list-disc pl-6 pb-3">
-                    <li className="pt-3">After writing it select the command and then click on the second bolt like
+                <ul className="text-2xl list-disc pl-6 pb-5">
+                    <li className="pt-3">After writing it select the code and then click on the second bolt like
                         icon at the top of Workbench.
                     </li>
+                </ul>
+
+                <div className="text-2xl pt-1 pl-3 border-l-4 border-slate-700"><span className="font-bold">Note : </span>
+                    The second bolt icon is used to run a selected portion of the code while the first one runs the whole file.
+                    We will mostly use the second one for running our queries. You can use the first one only when you have not ran a single code
+                    from that file, unless it will give error.</div>
+
+                <ul className="text-2xl list-disc pl-6 pb-3 pt-2">
                     <li className="pt-3">Then you can referesh the schemas section on the left of the Workbench
                         and then you will see your database being created. As you can see below :
                     </li>
@@ -175,10 +183,10 @@ export function Page4() {
 
                 <ul className="text-2xl list-disc pl-6 pb-3">
                     <li className="pt-7">So congratulations you have created your first database successfully.</li>
-                    <li className="pt-3">But to use this database you have to run this command : </li>
+                    <li className="pt-3">But to use this database to perform your queires you have to run this code : </li>
                 </ul>
 
-                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md my-3 w-[800px]">
                     USE xyz;
                 </div>
 
@@ -196,15 +204,16 @@ export function Page4() {
                     <li className="pt-3">But if you want to delete this database you can simply do it by using this command :</li>
                 </ul>
 
-                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md my-3 w-[800px]">
                     DROP DATABASE xyz;
                 </div>
 
                 <ul className="text-2xl list-disc pl-6 pb-3">
-                    <li className="pt-3">You can still create it using the same command we used to create database.</li>
+                    <li className="pt-3">You can still create the same database it using the same code
+                        we used to create database earlier.</li>
                 </ul>
 
-                <div className="text-2xl pt-4 py-7">Now let's learn the most important part of databases
+                <div className="text-2xl pt-3 pb-6">Now let's learn the most important part of databases
                     : Tables in the next page.</div>
             </div>
         </div>
@@ -236,7 +245,7 @@ export function Page6() {
 
                 <ul className="text-2xl list-disc pl-6 pb-2">
                     <li className="pt-5">We can add as much as columns as we want.</li>
-                    <li className="pt-3">So let's create our first table. Write the follwing code in your
+                    <li className="pt-3">So let's create our first table in our database. Write the follwing code in your
                         MySQL Workbench :
                     </li>
                 </ul>
@@ -268,7 +277,7 @@ export function Page6() {
                         into it in the upcoming pages.</li>
                 </ul>
 
-                <div className="text-2xl pt-4 py-7">Now let's learn about the datatypes used in SQL in the next page.</div>
+                <div className="text-2xl pt-3 pb-6">Now let's learn about the datatypes used in SQL in the next page.</div>
             </div>
         </div>
     )
@@ -291,7 +300,34 @@ export function Page8() {
         <div className="min-h-full w-full px-4">
             <h1 className="text-8xl font-semibold pt-3 text-slate-100">SQL Commands</h1>
 
-            <div className="text-gray-200 pt-10"></div>
+            <div className="text-gray-200 pt-10">
+                <div className="text-4xl font-semibold">Let's understand the types SQL Commands</div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">
+                        <span className="font-bold">DDL </span>
+                        (Data Definition Language) : create, alter, rename, truncate & drop
+                    </li>
+                    <li className="pt-7">
+                        <span className="font-bold">DQL </span>
+                        (Data Query Language) : select
+                    </li>
+                    <li className="pt-7">
+                        <span className="font-bold">DML </span>
+                        (Data Manipulation Language) : select, insert, update & delete
+                    </li>
+                    <li className="pt-7">
+                        <span className="font-bold">DCL </span>
+                        (Data Control Language) : grand & revoke permission to users
+                    </li>
+                    <li className="pt-7">
+                        <span className="font-bold">TCL </span>
+                        (Transaction COntrol Language) : start transaction, commit, rollback etc.
+                    </li>
+                </ul>
+
+                <div className="text-2xl pt-3 pb-6">Now let's learn about more database realted queries on the next page.</div>
+            </div>
         </div>
     )
 }
@@ -363,6 +399,9 @@ export function Page9() {
                 <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
                     SHOW TABLES;
                 </div>
+
+                <div className="text-2xl pt-5 pb-6">Now let's learn about the some more Table related queries
+                    in the next page.</div>
             </div>
         </div>
     )
@@ -374,7 +413,96 @@ export function Page10() {
         <div className="min-h-full w-full px-4">
             <h1 className="text-8xl font-semibold pt-3 text-slate-100">Table Related Queries</h1>
 
-            <div className="text-gray-200 pt-10"></div>
+            <div className="text-gray-200 pt-10">
+                <div className="text-4xl font-semibold">Let's see some more table related queries</div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">We created our first table using the follwing code :</li>
+                </ul>
+
+                <pre className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px] overflow-x-auto">
+                    <code>
+                        CREATE TABLE student ( {"\n"}
+                        &nbsp;&nbsp;id INT PRIMARY KEY, {"\n"}
+                        &nbsp;&nbsp;name VARCHAR(50), {"\n"}
+                        &nbsp;&nbsp;age INT NOT NULL {"\n"}
+                        );
+                    </code>
+                </pre>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Now to check our table which we created, we can run this following code :</li>
+                </ul>
+
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                    SELECT * FROM student;
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Now you will see a result gird opened where you can see your table. As
+                        you can see below :</li>
+                </ul>
+
+                <div className="pt-3 pl-3 overflow-hidden">
+                    <img className="rounded-lg" src="/images/img4.png" alt="workbench preview"></img>
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">But currently the values are null, which means there is no values inserted
+                        in the table yet.</li>
+                    <li className="pt-3">Let's insert some values in this table. You can insert values in the
+                        table using the follwing syntax :</li>
+                </ul>
+
+                <pre className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px] overflow-x-auto">
+                    <code>
+                        INSERT INTO table_name {"\n"}
+                        (colname1, colname2) {"\n"}
+                        VALUES {"\n"}
+                        (col1_v1, col2_v1), {"\n"}
+                        (col1_v2, col2_v2);
+                    </code>
+                </pre>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Let's understand this syntax with an example code :</li>
+                </ul>
+
+                <pre className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px] overflow-x-auto">
+                    <code>
+                        INSERT INTO student {"\n"}
+                        (id, name, age) {"\n"}
+                        VALUES {"\n"}
+                        (101, "karan", 20), {"\n"}
+                        (102, "arjun", 21);
+                    </code>
+                </pre>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">After writing this code select it all and run it. Check the action output tab below.
+                        If it shows green tick we are good to go.</li>
+                    <li className="pt-3">Now we can check the inserted data using the same code :</li>
+                </ul>
+
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                    SELECT * FROM student;
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Now you must be seeing the follwing result in the result gird :</li>
+                </ul>
+
+                <div className="pt-3 pl-3 overflow-hidden">
+                    <img className="rounded-lg" src="/images/img5.png" alt="workbench preview"></img>
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Which means the data has been successfully inserted. Congratualtions you have not
+                        learned to create your first table but also inserted the data to it.</li>
+                </ul>
+
+                <div className="text-2xl pt-5 pb-6">Now let's learn about SQL Keys in the next page.</div>
+            </div>
         </div>
     )
 }
@@ -407,7 +535,68 @@ export function Page13() {
         <div className="min-h-full w-full px-4">
             <h1 className="text-8xl font-semibold pt-3 text-slate-100">SQL Select</h1>
 
-            <div className="text-gray-200 pt-10"></div>
+            <div className="text-gray-200 pt-10">
+                <div className="text-4xl font-semibold">Let's understand SELECT Query in detail</div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">SELECT is used to select any data from the database.</li>
+                    <li className="pt-7">We used SELECT earlier to print the whole table. We used the follwing code :</li>
+                </ul>
+
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                    SELECT * FROM student;
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7 list-none">Let's understand this code :</li>
+                    <li className="pt-3">So here we know about "SELECT", this symbol "*" means all. So SELECT * means
+                        Select all.</li>
+                    <li className="pt-3">"FROM" is used to define from where we want to select data.</li>
+                    <li className="pt-3">And at last "student" is the table which we created and want to select data from.</li>
+                    <li className="pt-3">So this whole code means select all data from table student. As a result is shows
+                        whole table.</li>
+                    <li className="pt-7">But what if we wanted to just select only specific columns from the given table.
+                        We can do this with the follwing syntax :</li>
+                </ul>
+
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                    SELECT col1, col2 FROM table_name;
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Let's understand this with an example :</li>
+                </ul>
+
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                    SELECT id, name FROM student;
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Select this code and run it. You will see the follwing result :</li>
+                </ul>
+
+                <div className="pt-3 pl-3 overflow-hidden">
+                    <img className="rounded-lg" src="/images/img6.png" alt="workbench preview"></img>
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">So what we did? Rather than selecting thw whole table, we selected only few
+                        specific columns (id, name).</li>
+                    <li className="pt-3">if you want to just select only one column, you can do that as well like this :</li>
+                </ul>
+
+                <div className="bg-gray-800 text-green-400 font-mono p-3 pl-5 rounded-md mt-3 w-[800px]">
+                    SELECT name FROM student;
+                </div>
+
+                <ul className="text-2xl list-disc pl-6 pb-2">
+                    <li className="pt-7">Now you must be seeing the name column only. You can do the same for id
+                        and age columns.</li>
+                </ul>
+
+                <div className="text-2xl pt-5 pb-6">Now let's learn about Where Clause in the next page.</div>
+
+            </div>
         </div>
     )
 }
